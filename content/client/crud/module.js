1a52fef963e248203c7fd603caa83644b4461783
Nina Tran <ninatranschacc@gmail.com>
2017-11-27 14:11:28 -0800

    asdf

--- content/client/crud/module.js
+++ content/client/crud/module.js
@@ -1,22 +1,5 @@
 /* global angular */
 (function () {
     'use strict';
-
     angular.module('client.crud', [])
-    //temp route
-    angular.module('client.crud').config(RouteConfig);
-    RouteConfig.$inject = ["$stateProvider"];
-    function RouteConfig($stateProvider) {
-        $stateProvider
-            .state('site.navbar', {
-                url: '/navbar',
-                views: {
-                    'content@site': {
-                        templateUrl: 'client/crud/navbar/navbar.html',
-                        controller: 'navbarController as navbarCtrl'
-                    }
-                }
-            })
-    }
-    
 })();
