f9d3e607b4e9320bbed9854ae4dfead225aacff3
David Vergona <dsvergona@gmail.com>
2017-11-27 17:07:59 -0800

    More html

--- content/client/crud/profile/profile.controller.js
+++ content/client/crud/profile/profile.controller.js
@@ -2,20 +2,29 @@
 (function () {
     'use strict'
     angular.module('client.crud')
-    .component('userProfile', {
-        templateUrl: 'client/crud/profile/profile.html',
-        controller: 'profileController as pCtrl'
+        .component('userProfile', {
+            templateUrl: 'client/crud/profile/profile.html',
+            controller: 'profileController as pCtrl'
 
-    })
+        })
 
     angular.module('client.crud')
         .controller('profileController', ProfileController)
 
-        ProfileController.$inject = ['$log']
+    ProfileController.$inject = ['$log']
 
     function ProfileController($log) {
         var vm = this
-        vm.$onInit=_init()
+        vm.$onInit = _init()
+
+        vm.profile = {
+            name: "John Doe",
+            age: "22",
+            sex: "M",
+        }
+        vm.group = {
+            members:["Tim","Aya","Ed"]
+        }
 
         function _init() {
 
