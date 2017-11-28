1c490833dd8cc1bbbe9c7b8af3652f83c8a667ac
Tiffany Nguyen <thnguye7@uci.edu>
2017-11-28 06:36:32 -0800

    fixed showing links based on profile status and options tweaks to topView

--- content/client/layout/layout.controller.js
+++ content/client/layout/layout.controller.js
@@ -2,14 +2,19 @@
     angular.module('client.layout')
         .controller('layoutController', LayoutController)
 
-    LayoutController.$inject = []
-    function LayoutController() {
+    LayoutController.$inject = ['$state']
+    function LayoutController($state) {
         var vm = this
         
 
         init()
 
         function init() {
+            if($state.current.name == "site.profile"){
+                vm.hideLoginReg = false
+            } else {
+                vm.hideLoginReg = true
+            }
         }
         
         
