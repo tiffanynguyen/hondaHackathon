447670aef682cf28f71112eff4a35e7623f61ed8
Nina Tran <ninatranschacc@gmail.com>
2017-11-28 06:58:51 -0800

    travelgroup mostly done

--- content/client/crud/travelgroups/travelgroup.controller.js
+++ content/client/crud/travelgroups/travelgroup.controller.js
@@ -1,21 +1,27 @@
 (function(){
     "use strict";
+    angular
+        .module("client.crud")
+        .component("travelgroupDetails", {
+            templateUrl: "client/crud/travelgroups/travelgroup.html",
+            controller: "travelGroupController"
+        })
+})();
+
+(function(){
+    "use strict";
     angular 
         .module("client.crud")
-        .controller("travelGroupsController", TravelGroupsController);
+        .controller("travelGroupController", TravelGroupController);
     
-    TravelGroupsController.$inject = [];
+    TravelGroupController.$inject = [];
 
-    function TravelGroupsController() {
+    function TravelGroupController() {
         var vm = this;
 
-        vm.create = false;
-        vm.openGroupDetails = false;
-        vm.newItem = {};
-
+        vm.edit = false;
         vm.item = {
             name: "Sports Party",
-            role: "Organizer",
             event: "Super Bowl 2018",
             description: "We're partying at the Super Bowl for the year 2018 (terribly early planning) ! Come join us for the fun! We are meeting at 120 Newport Center Dr at Newport Beach. It'll be a wonderful 4-day road trip. Prepare yourselves!",
             numPeople: 6,
@@ -43,7 +49,6 @@
         vm.myTravelGroups = [
             {
                 name: "Sports Party",
-                role: "Organizer",
                 event: "Super Bowl 2018",
                 numPeople: 6,
                 transportation: "Car",
@@ -52,8 +57,7 @@
 
             },
             {
-                name: "Weab Life",
-                role: "Participant",
+                name: "Weeb Life",
                 event: "Anime Expo 2018",
                 numPeople: 12,
                 transportation: "Train",
@@ -61,25 +65,6 @@
                 location: "1201 S Figueroa St, Los Angeles, CA 90015"
             }
         ];
-
-        vm.setGroup = _setGroup;
-        function _setGroup(){
-            if (vm.openGroupDetails == true) {
-                vm.openGroupDetails = false;
-                // $("#travelgroupview").addClass("hidden");
-            } else {
-                vm.openGroupDetails = true;
-                // $("#travelgroupview").removeClass("hidden");
-            }
-        }
-
-        vm.pushNewItem = _pushNewItem;
-        function _pushNewItem(){
-            vm.newItem.role = "Organizer";
-            var item = angular.copy(vm.newItem);
-            vm.myTravelGroups.push(item);
-            vm.newItem = {};
-        }
 
     }
     
